f2e419858c1465d4381766f6055c4b879d581e00
Angel Ramirez <pepe_199904@hotmail.com>
2021-02-15 20:23:31 -0600

    Subida del sitio INICIO copia del repositorio de Dan

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="absolute w-10 inset-y-0 left-4 text-white">
+      Hola
+    </header>
+  );
+}
+
+export default Header;
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      spacing: {
+        '40px': '40px'
+      },
+      colors: {
+        themePurple: '#7733FF',
+        themeBlue: '#3399FF',
+        themeDarkBlue: '#151B38'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
